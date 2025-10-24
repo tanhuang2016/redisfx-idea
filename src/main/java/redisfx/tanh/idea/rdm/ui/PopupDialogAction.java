71bf06b39afbc5a8f8d016114d6e983003205f66
1103617438@qq.com <1103617438@qq.com>
2025-10-24 17:02:40 +0800

    feat(plugin): 添加弹窗对话框操作功能
    - 在 plugin.xml 中注册新的动作菜单项- 实现 PopupDialogAction 类处理动作事件- 添加快捷键 Ctrl+T 触发弹窗
    - 弹窗显示当前选中元素信息
    - 根据项目是否打开控制动作可见性

---
+++ src/main/java/redisfx/tanh/idea/rdm/ui/PopupDialogAction.java
@@ -0,0 +1,38 @@
+package redisfx.tanh.idea.rdm.ui;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
+
+public class PopupDialogAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        System.out.println(123);
+        Project currentProject = event.getProject();
+        StringBuilder message =
+                new StringBuilder(event.getPresentation().getText() + " Selected!");
+        // If an element is selected in the editor, add info about it.
+        Navigatable selectedElement = event.getData(CommonDataKeys.NAVIGATABLE);
+        if (selectedElement != null) {
+            message.append("\nSelected Element: ").append(selectedElement);
+        }
+        String title = event.getPresentation().getDescription();
+        Messages.showMessageDialog(
+                currentProject,
+                message.toString(),
+                title,
+                Messages.getInformationIcon());
+        // TODO: insert action logic here
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        // Set the availability based on whether a project is open
+        Project project = e.getProject();
+        e.getPresentation().setEnabledAndVisible(project != null);
+    }
+}
