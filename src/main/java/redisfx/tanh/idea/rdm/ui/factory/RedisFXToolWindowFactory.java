796f77a04a2b0b8aeccb3bf50451d03d97578d43
hod <1103617438@qq.com>
2025-10-26 20:23:53 +0800

    feat(plugin): 添加 RedisFX 工具窗口支持
    - 在 plugin.xml 中注册了 RedisFX 工具窗口
    - 创建了 RedisFXToolWindowFactory 工厂类
    - 实现了工具窗口内容创建方法- 设置工具窗口显示在右侧主组栏- 使用 IntelliJ 的数据库对象图标作为工具窗口图标

---
+++ src/main/java/redisfx/tanh/idea/rdm/ui/factory/RedisFXToolWindowFactory.java
@@ -0,0 +1,21 @@
+package redisfx.tanh.idea.rdm.ui.factory;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class RedisFXToolWindowFactory  implements ToolWindowFactory {
+
+    /**
+     * 创建窗口内容
+     * 当用户点击工具窗口按钮时createToolWindowContent()工厂类的方法被调用并初始化工具窗口的用户界面。
+     * 此操作可确保未使用的工具窗口在启动时间或内存使用时不产生任何开销:如果用户未与工具窗口交互,则不会加载或执行任何插件代码
+     * @param project
+     * @param toolWindow
+     */
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        System.out.println(project.getName());
+    }
+}
