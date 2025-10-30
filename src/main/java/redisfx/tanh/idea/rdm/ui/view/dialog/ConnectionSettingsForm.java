2ec73c9bd7c81ef3b12065b6a27e119bf73cd4f4
1103617438@qq.com <1103617438@qq.com>
2025-10-30 15:24:38 +0800

    refactor(ui):重构连接设置对话框UI结构
    - 调整对话框初始尺寸为100x100
    - 更新表单根面板绑定从panel1到rootPane
    - 修改标签面板绑定从tabbedPane1到tabbedPane
    - 添加滚动面板generalScrollPane并设置无边框
    - 优化getContent方法返回根面板并移除视口边框注释

--- src/main/java/redisfx/tanh/idea/rdm/ui/view/dialog/ConnectionSettingsForm.java
+++ src/main/java/redisfx/tanh/idea/rdm/ui/view/dialog/ConnectionSettingsForm.java
@@ -3,16 +3,19 @@ package redisfx.tanh.idea.rdm.ui.view.dialog;
 import javax.swing.*;
 
 public class ConnectionSettingsForm {
-    private JTabbedPane tabbedPane1;
-    private JPanel panel1;
+    private JTabbedPane tabbedPane;
+    private JPanel rootPane;
     private JTextField name;
     private JTextField host;
     private JTextField port;
     private JTextField auth;
     private JCheckBox clusterCheckBox;
+    private JScrollPane generalScrollPane;
 
 
     public JComponent getContent() {
-        return panel1;
+        generalScrollPane.setBorder(null);
+//        generalScrollPane.setViewportBorder(null);
+        return rootPane;
     }
 }
