25199426c67115cb15490b590d08216247f4d34d
1103617438@qq.com <1103617438@qq.com>
2025-10-30 16:27:25 +0800

    feat(ui): 实现哨兵配置面板的显示控制
    -为哨兵面板添加绑定ID "sentinelPanel"- 设置哨兵面板初始状态为不可见
    - 添加复选框状态变更监听器- 根据哨兵复选框选中状态动态控制面板可见性

--- src/main/java/redisfx/tanh/idea/rdm/ui/view/dialog/ConnectionSettingsForm.java
+++ src/main/java/redisfx/tanh/idea/rdm/ui/view/dialog/ConnectionSettingsForm.java
@@ -3,6 +3,8 @@ package redisfx.tanh.idea.rdm.ui.view.dialog;
 import com.intellij.ui.components.JBScrollPane;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class ConnectionSettingsForm {
     private JTabbedPane tabbedPane;
@@ -17,7 +19,18 @@ public class ConnectionSettingsForm {
     private JCheckBox sentinelCheckBox;
     private JTextField masterName;
     private JPasswordField passwordField1;
+    private JPanel sentinelPanel;
 
+
+    public ConnectionSettingsForm() {
+        sentinelCheckBox.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                boolean isSelected = sentinelCheckBox.isSelected();
+                sentinelPanel.setVisible(isSelected);
+            }
+        });
+    }
 
     public JComponent getContent() {
         return rootPane;
